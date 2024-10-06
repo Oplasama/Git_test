f1a583ea1ad6f99ff1e931a57163cbe575878c84
Quan Nguyen <nguyenminhquan180802@gmail.com>
2024-10-06 15:37:29 +0300

    ex10

--- Git_ex4.py
+++ Git_ex4.py
@@ -1,9 +1,5 @@
-#file name:Git_ex4 (Git course, exercise 4)
-#Author: Quan Nguyen
-#purpose: Git testing
-
 print("I am a beginner without exp related to Git")
 
 print("this line is for exercise 8")
 
-print("this is for ex 10")
+print("different line for ex 10")
